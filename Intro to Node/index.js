b63ec07bd1a83a2d791c06164452ced5720239b7
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2020-07-28 16:56:49 -0400

    Module 18

---
+++ Intro to Node/index.js
@@ -0,0 +1,7 @@
+// // Internal Node JS module
+// const fs = require('fs');
+// fs.copyFileSync("file1.txt", "file2.txt");
+
+// External Node JS module
+const superheroes = require('superheroes');
+console.log(superheroes.random());
